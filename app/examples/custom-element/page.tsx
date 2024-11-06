fe3cf99eb1e480ee43289f0a3650e89271c76c39
Uğur Oruç <uoruc5@hotmail.com>
2024-11-06 13:44:50 +0100

    disable: ssr for custom element

--- app/examples/custom-element/page.tsx
+++ app/examples/custom-element/page.tsx
@@ -1,35 +1,9 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { useEffect } from "react";
+const Component = dynamic(() => import("./custom-element"), {
+  ssr: false,
+});
 
-// Define a custom element
-class MyCustomElement extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = "<p>This is a custom element from React</p>";
-  }
-}
-
-// Register the custom element
-if (typeof customElements !== "undefined") {
-  customElements.define("my-custom-element", MyCustomElement);
-}
-
-export default function Component() {
-  useEffect(() => {
-    // Ensure the custom element is defined
-    if (
-      typeof customElements !== "undefined" &&
-      !customElements.get("my-custom-element")
-    ) {
-      customElements.define("my-custom-element", MyCustomElement);
-    }
-  }, []);
-
-  return (
-    <div className="p-4 bg-white rounded-lg shadow">
-      <h2 className="text-2xl font-bold mb-4">Custom Element Example</h2>
-      {/* @ts-expect-error type is not defined for custom element*/}
-      <my-custom-element />
-    </div>
-  );
+export default function Page() {
+  return <Component />;
 }
